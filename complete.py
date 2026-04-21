2f056f882d5992136e80393d166a4d011cda299a
Habib <habibsaddique4410@gmail.com>
2026-04-21 22:58:40 +0500

    Added feature test

---
+++ complete.py
@@ -0,0 +1,32 @@
+# Question 13 — [3 Marks]
+cart = []
+# a) Empty cart
+# YOUR CODE HERE
+cart.append("Pen")
+cart.append("Eraser")
+cart.append("Ruler")
+cart.append("Marker")
+# b) Add items one by one
+# YOUR CODE HERE
+print(cart)
+cart[cart.index("Pen")] = "Blue Pen"
+print(cart)
+
+# c) Print cart and count
+# YOUR CODE HERE
+
+
+# d) Remove "Eraser"
+# YOUR CODE HERE
+
+
+# e) Insert "Calculator" at index 1
+# YOUR CODE HERE
+
+
+# f) Update "Pen" to "Blue Pen"
+# YOUR CODE HERE
+
+
+# g) Print final cart in alphabetical order
+# YOUR CODE HERE
